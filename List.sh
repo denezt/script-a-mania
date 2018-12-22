a8e59e31394b507a8bfbecd2e32d76d88b73a03d
Richard L. Jackson <denezt@users.noreply.github.com>
2018-12-22 23:51:03 +0100

    Update List.sh

--- List.sh
+++ List.sh
@@ -1,3 +1,4 @@
 size=1 ; while ! stat conn.log ; do echo $size ; bro -e "redef encap_hdr_size=$size;" -r network-dump.pcap ; size=$((size+1)) ; done # Keep i
 pdf2txt cv.pdf | grep -C3 -i phone
 for i in IMG_3[0-4]*.JPG ; do convert -quality 60 -geometry 300 "$i" "thumbs/$i" ; done # Make thumbnails of images IMG_3000.JPG - IMG_3499.JPG
+rsync -a -delete empty/ foo/ # Apparently the fastest way to delete millions of small files
