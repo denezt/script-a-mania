25fa1dfc50a3c113f8b463d62da819eca200c1d1
Richard L. Jackson <denezt@users.noreply.github.com>
2018-12-23 06:36:51 +0100

    Update List.sh

--- List.sh
+++ List.sh
@@ -2,3 +2,4 @@ size=1 ; while ! stat conn.log ; do echo $size ; bro -e "redef encap_hdr_size=$s
 pdf2txt cv.pdf | grep -C3 -i phone
 for i in IMG_3[0-4]*.JPG ; do convert -quality 60 -geometry 300 "$i" "thumbs/$i" ; done # Make thumbnails of images IMG_3000.JPG - IMG_3499.JPG
 rsync -a -delete empty/ foo/ # Apparently the fastest way to delete millions of small files
+vim scp://user@server1//etc/httpd/httpd.conf 
