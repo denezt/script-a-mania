5d9b12a36722963dc72ceecf2b9e0c62333bea82
Richard L. Jackson <denezt@users.noreply.github.com>
2018-12-25 06:56:46 +0100

    Update List.sh

--- List.sh
+++ List.sh
@@ -3,3 +3,4 @@ pdf2txt cv.pdf | grep -C3 -i phone
 for i in IMG_3[0-4]*.JPG ; do convert -quality 60 -geometry 300 "$i" "thumbs/$i" ; done # Make thumbnails of images IMG_3000.JPG - IMG_3499.JPG
 rsync -a -delete empty/ foo/ # Apparently the fastest way to delete millions of small files
 vim scp://user@server1//etc/httpd/httpd.conf 
+pv bigdump.sql.gz | gunzip | mysql
