31a6155d62f4decf640a5e9415a2bacb3d56f3c7
Richard L. Jackson <denezt@users.noreply.github.com>
2019-07-12 18:12:51 +0200

    Update command_list.sh

--- command_list.sh
+++ command_list.sh
@@ -51,9 +51,8 @@ cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="
   print_r($_FILES);
   print "</pre>";
 ?>
-
-
-
+# View Post Packages
+tcpdump -i wlan0 -s 0 -A 'tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x504F5354'
 
 
 
