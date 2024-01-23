64b586f9d277305588525943ee635db0fdc9e74f
Richard Jackson <richard.jackson.extern@bdr.de>
2024-01-23 13:57:16 +0100

    Adding, a new interact swapfile tool

--- system_tools/swapfiles/ex2/swapfile_manager.sh
+++ system_tools/swapfiles/ex2/swapfile_manager.sh
@@ -3,6 +3,11 @@
 _allowed_swapsize=60
 config="config.settings"
 
+logger(){
+	input_log="${1}"
+	printf "[ $(date '+%F') ] ${input_log}\n"
+}
+
 error(){
 	printf "\033[35mError:\t\033[31m${1}!\033[0m\n"
 	exit 1
@@ -21,9 +26,24 @@ parse_types(){
 	esac
 }
 
+create_config(){
+	option="${1}"
+	parameter="${2}"
+	case $option in
+		append)
+		if [ -n "${parameter}" ]; then
+			echo "${parameter}" | tee -a "${config}"
+		else
+			error "Missing parameter for configuration ${config}"
+		fi
+		;;
+		flush|initialize) printf "" > ${config};;
+	esac
+}
+
 test_disk_size(){
 	_dirname="${1}"
-	_swapsize="${2}"
+	declare -i _swapsize="${2}"
 	if [ -d "${_dirname}" ];
 	then
 		curr_disk_size=$(df -h "${_dirname}" | tail -n 1 | awk '{print $4}' | tr -d [:alpha:])
@@ -41,28 +61,36 @@ test_disk_size(){
 	fi
 }
 
+# Defines and will parse the values
 define_settings(){
 	# Set the root directory
-	read -p "Enter main directory location for swapfile: " _dir
-	dir="${_dir}"
+	read -p "Enter main directory location for swapfile: [default: /] " _dir
+	[ -z "${_dir}" ] && _dir='/'
 
 	# Define the swapfile name
-	read -p "What do you want to name the swapfile? (No leading '/'): " _name
-	swapfile="${_name}"
+	read -p "What do you want to name the swapfile? [swapfile_001] " _name
+	[ -z "${_name}" ] && _name='swapfile_001'
 
 	# Define the size of the swapfile
-	read -p "Define the size of the swapfile: " _size
-	parse_types $_size 'number'
-	swap_size="${_size}"
+	read -p "Define the size of the swapfile. [1GB]: " _size
+	[ -z "${_size}" ] && _size=1
+	_size_number=$(printf $_size | sed "s|[A-Za-z]||g")
+	parse_types $_size_number 'number'
 
 	# Ensure that the usage size is under the max allowed percentage
 	test_disk_size "${_dir}" "${swap_size}"
 
 	# Define the rate of swapping
-	read -p "Define frequency of swapping: " _rate
+	read -p "Define frequency of swapping [default is 60]: " _rate
+	[ -z "${_rate}" ] && _rate=60
 	parse_types $_rate 'number'
-	swappiness="${_rate}"
 
+	# Enter values into the config file
+	create_config 'initialize'
+	create_config 'append' "dir='${_dir}'"
+	create_config 'append' "swapfile='${_name}'"
+	create_config 'append' "declare -i swap_size='${_size}'"
+	create_config 'append' "declare -i swappiness='${_rate}'"
 }
 
 modify_settings(){
@@ -82,36 +110,68 @@ modify_settings(){
 }
 
 create_swapfile(){
-	source config.settings
+	if [ -e "$config" ];
+	then
+		source $config
+		logger "Creating, swapfile with settings:\n"
+		cat "${config}"
 
-	# Check Current Swap
-	sudo swapon -s
+		swapfile_path="${dir}/${swapfile}"
+		# Check Current Swap
+		sudo swapon -s
 
-	# Create Swap File
-	sudo fallocate -v -l 4G /swapfile
-	chmod 600 /swapfile
+		# Create Swap File
+		logger "Creating, swap file"
+		sudo dd if=/dev/zero of=${swapfile_path} bs=1024 count=$(echo "${swap_size} * 1024000" | bc)
+		sudo chmod 600 ${swapfile_path}
+		# Make it to swap format and activate on your system
+		sudo mkswap ${swapfile_path}
+		sudo swapon ${swapfile_path}
+		logger "Successfully, created swap file ${swapfile_path}"
 
-	# Make it to swap format and activate on your system
-	sudo mkswap /swapfile
-	sudo swapon /swapfile
+		# Make Swap Permanent
+		logger "Making, swap persistent"
+		if [ -n "$(grep -o ${swapfile_path} /etc/fstab)" ];
+		then
+			# Overwrite following fstab entry.
+			old_value="$(grep ${swapfile_path} /etc/fstab)"
+			new_value="${swapfile_path}   none    swap    sw    0   0"
+			sudo sed -i "s|${old_value}|${new_value}|g" /etc/fstab
+		else
+			# Append following fstab entry.
+			echo "${swapfile_path}   none    swap    sw    0   0" | sudo tee -a /etc/fstab
+		fi
 
-	# Make Swap Permanent
-	sudo vim /etc/fstab
-	# and add below entry to end of file
-	/swapfile   none    swap    sw    0   0
-
-	# Check System Swap Memory
-	sudo swapon -s
-	free -m
-
-	# Update Swappiness Parameter
-	sudo vim /etc/sysctl.conf
-	# append following configuration to end of file
-	vm.swappiness=10
-
-	# Now reload the sysctl configuration file
-	sudo sysctl -p
+		# Check System Swap Memory
+		sudo swapon -s
+		free -m
+		# Update Swappiness Parameter
+		if [ -n "$(grep -o 'vm.swappiness' /etc/sysctl.conf)" ];
+		then
+			# Overwrite following configuration setting.
+			old_value="$(grep 'vm.swappiness' /etc/sysctl.conf)"
+			new_value="vm.swappiness=${swappiness}"
+			sudo sed -i "s|${old_value}|${new_value}|g" /etc/sysctl.conf
+		else
+			# append following configuration to end of file
+			echo "vm.swappiness=${swappiness}" | sudo tee -a /etc/sysctl.conf
+		fi
+		# Now reload the sysctl configuration file
+		sudo sysctl -p
+	else
+		error "Missing or unable to find $config file"
+	fi
 }
 
-modify_settings
 
+for argv in $@
+do
+	case $argv in
+		--modify|--create-config) _modify_config='true';;
+		--create|--create-swap) _create_swap='true';;
+		*) error "Missing or invalid parameter was given";;
+	esac
+done
+
+[ "${_modify_config}" = 'true' ] && modify_settings
+[ "${_create_swap}" = 'true' ] && create_swapfile
