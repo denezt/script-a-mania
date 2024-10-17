930073dc9fdb91b4876861cf47a75a6cb78fe84c
Richard Jackson <denezt@yahoo.com>
2024-10-17 23:16:26 +0200

    Update java 8 to 21 update

--- updating_tools/java8_to_java21.sh
+++ updating_tools/java8_to_java21.sh
@@ -4,9 +4,12 @@
 # Define session directory
 dirname='java8to21'
 
-# Define logfile
+# Define CSV filename
 _date="$(date '+%s')"
-logfile="analysis_results-${_date}.txt"
+filename="analysis_data-${_date}"
+csvfile="${dirname}/${filename}.csv"
+logfile="${dirname}/${filename}.log"
+refactor="${dirname}/${filename}.refr"
 
 error(){
     printf "\033[35mError:\t\033[31m${1}\033[0m\n"
@@ -15,7 +18,7 @@ error(){
 
 parse_project_dir(){
     PROJECT_DIR=$1
-    if  [ ! -d "${PROJECT_DIR}" ];
+    if  [ ! -d "$PROJECT_DIR" ];
     then
         error "Unable to locate \'${PROJECT_DIR}\'"
     fi
@@ -24,7 +27,7 @@ parse_project_dir(){
 write_to_log(){
     [ ! -d "${dirname}" ] && mkdir -v "${dirname}"
     log_entry="${1}"
-    echo "${log_entry}" | tee -a ${dirname}/$logfile
+    printf "[ $(date '+%F %T') ] ${log_entry}\n" | tee -a $logfile
 }
 
 # Looking for any deprecated packages
@@ -32,10 +35,14 @@ find_deprecated(){
     PROJECT_DIR=$1
     write_to_log "------------------------------------------------"
     write_to_log "Checking for deprecated APIs..."
-    for f in $(egrep -r --include="*.java" "@Deprecated" "$PROJECT_DIR");
-    do
-        write_to_log "${f}"
-    done
+    if [ -n "$(egrep -r --include='*.java' '@Deprecated' $PROJECT_DIR)" ];
+    then
+        # Stream into CSV file.
+        echo "Checking for deprecated APIs" | tee -a $refactor
+        egrep -r --include='*.java' '@Deprecated' "$PROJECT_DIR" | tee -a $refactor
+    else
+        write_to_log "No deprecated APIs were found"
+    fi
 }
 
 # Checking if javax packages exist
@@ -43,9 +50,10 @@ find_javax(){
     PROJECT_DIR=$1
     write_to_log "------------------------------------------------"
     write_to_log "Checking for javax package usage (potential migration needed to Jakarta EE)..."
-    if [ -n "$(egrep -r --include="*.java" "javax." "$PROJECT_DIR")" ];
+    if [ -n "$(egrep -r --include='*.java' 'javax.' $PROJECT_DIR)" ];
     then
-        egrep -r --include="*.java" "javax." "$PROJECT_DIR" | sed 's/^/\"/g' | sed 's/\:/\"\,\"/g' | sed 's/\;/\"/g' | xargs write_to_log
+        echo '"Checking for javax package usage (potential migration needed to Jakarta EE)...",""' | tee -a $csvfile
+        egrep -r --include='*.java' 'javax.' "$PROJECT_DIR" | sed 's/^/\"/g' | sed 's/\:/\"\,\"/g' | sed 's/\;/\"/g' | tee -a $csvfile
     fi
 }
 
@@ -54,10 +62,11 @@ find_api_internal(){
     PROJECT_DIR=$1
     write_to_log "------------------------------------------------"
     write_to_log "Checking for usage of internal APIs (sun.*)..."
-    for f in $(egrep -r --include="*.java" "sun." "$PROJECT_DIR");
-    do
-        write_to_log $f
-    done
+    if [ -n "$(egrep -r --include='*.java' 'sun.' $PROJECT_DIR)" ];
+    then
+        echo '"Checking for usage of internal APIs (sun.*)...",""' | tee -a $csvfile
+        egrep -r --include='*.java' 'sun.' $PROJECT_DIR | sed 's/^/\"/g' | sed 's/\:/\"\,\"/g' | sed 's/\;/\"/g' | tee -a $csvfile
+    fi
 }
 
 # Looking for reflective access (which might cause issues with Java Modules)
@@ -65,10 +74,11 @@ find_reflectives(){
     PROJECT_DIR=$1
     write_to_log "------------------------------------------------"
     write_to_log "Checking for reflective access usage (possible illegal reflective access in Java 9+)..."
-    for f in $(egrep -r --include="*.java" "Class.forName\|getDeclaredField\|setAccessible" "$PROJECT_DIR");
-    do
-        write_to_log $f
-    done
+    if [ -n "$(egrep -r --include='*.java' 'Class.forName\|getDeclaredField\|setAccessible' $PROJECT_DIR)" ];
+    then
+        echo 'Checking for reflective access usage (possible illegal reflective access in Java 9+)...' | tee -a $refactor
+        egrep -r --include='*.java' 'Class.forName\|getDeclaredField\|setAccessible' $PROJECT_DIR | tee -a $refactor
+    fi
 }
 
 # Check for refactoring older Java 8 contained 
@@ -76,10 +86,11 @@ find_refactor_candidates(){
     write_to_log "------------------------------------------------"
     write_to_log "Identifying usage of old Java language constructs (optional but refactor candidates)..."
     # Look for old-style for loops which could be replaced with Streams
-    for f in $(egrep -r --include="*.java" "for (.* : .*Iterable" "$PROJECT_DIR");
-    do
-        write_to_log $f
-    done
+    if [ -n "$(egrep -r --include='*.java' 'for (.* : .*Iterable' $PROJECT_DIR)" ];
+    then
+        echo 'Identifying usage of old Java language constructs (optional but refactor candidates)...' | tee -a $refactor
+        egrep -r --include='*.java' 'for (.* : .*Iterable' $PROJECT_DIR | tee -a $refactor
+    fi
 }
 
 # List the project's dependencies (checking for old versions)
@@ -93,10 +104,10 @@ list_project_dependencies(){
     [ -z "$(command -v gradle)" ] && error "Missing build automation program 'gradle'"
     if [ -f "$PROJECT_DIR/pom.xml" ]; then
         write_to_log "Found Maven project. Listing dependencies from pom.xml..."
-        for f in $(mvn -f "$PROJECT_DIR/pom.xml" dependency:tree);
-        do
-            write_to_log $f
-        done
+        if [ "$(mvn -f $PROJECT_DIR/pom.xml dependency:tree)" ];
+        then
+            mvn -f "$PROJECT_DIR/pom.xml" dependency:tree | tee -a $refactor
+        fi
     else
         write_to_log "No Maven POM (Project Object Model) file found. Please ensure you check your dependencies manually."
     fi
@@ -106,10 +117,10 @@ list_project_dependencies(){
     [ -z "$(command -v mvn)" ] && error "Missing build automation program 'mvn'"
     if [ -f "$PROJECT_DIR/build.gradle" ]; then
         write_to_log "Found Gradle project. Listing dependencies from build.gradle..."
-        for f in $(gradle --scan --info --no-daemon -p "$PROJECT_DIR" dependencies 2> /dev/null);
-        do
-            write_to_log $f
-        done
+        if [ -n "$(gradle --scan --info --no-daemon -p $PROJECT_DIR dependencies 2> /dev/null)" ];
+        then
+            gradle --scan --info --no-daemon -p $PROJECT_DIR dependencies 2> /dev/null | tee -a $refactor
+        fi
     else
         write_to_log "No Gradle build file found. Please ensure you check your dependencies manually."
     fi
@@ -122,10 +133,11 @@ find_jars(){
     PROJECT_DIR=$1
     write_to_log "------------------------------------------------"
     write_to_log "Identifying any JARs that might need module migration..."
-    for f in $(find "$PROJECT_DIR" -name "*.jar" -exec jdeps {} \;);
-    do
-        write_to_log $f
-    done
+    if [ -n "$(find $PROJECT_DIR -name "*.jar" -exec jdeps {} \;)" ];
+    then
+        find $PROJECT_DIR -name "*.jar" -exec jdeps {} \; | tee -a $refactor
+    fi
+
     write_to_log "------------------------------------------------"
     write_to_log "Scan complete. Review the results for changes needed before upgrading to Java 21."
 }
@@ -136,11 +148,11 @@ run_all_checks(){
     parse_project_dir "${PROJECT_DIR}"
     write_to_log "Checking Project: ${PROJECT_DIR}"
     echo "Scanning Java 8 project for issues before migrating to Java 21..." 
-    # find_deprecated "${PROJECT_DIR}"
+    find_deprecated "${PROJECT_DIR}"
     # find_javax "${PROJECT_DIR}"
     # find_api_internal "${PROJECT_DIR}"
     # find_reflectives "${PROJECT_DIR}"
-    find_refactor_candidates "${PROJECT_DIR}"
+    # find_refactor_candidates "${PROJECT_DIR}"
     # Apply when build tool is set
     if [ -n "${BUILD_TOOL}" ];
     then
