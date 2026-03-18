993e477778c3ff773c32454eefdb5538ff6b4d53
Richard Jackson <denezt@yahoo.com>
2026-03-18 13:13:28 +0100

    Adding readme and usage files for resource optimizer

--- system_tools/powershell/resource_optimizer_scheduler.ps1
+++ system_tools/powershell/resource_optimizer_scheduler.ps1
@@ -1,7 +1,7 @@
 
 
 
-$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File .\resource_optimizer.ps1"
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File ${HOME}\resource_optimizer.ps1"
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
 
 Register-ScheduledTask -TaskName "ResourceOptimizer" -Action $Action -Trigger $Trigger -Description "Keeps system activity alive, and prevents idle status by sending a key press every 4 minutes." -User "SYSTEM" -RunLevel Highest
